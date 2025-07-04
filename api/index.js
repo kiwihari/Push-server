e4bd6fafc3075dc6ff6861ad3fe5c95348e2aaa8
ikumba01 <kumbalathara.ishara@ucy.ac.cy>
2025-07-04 10:21:09 +0300

    Added logs to  index file-debug

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
 
+console.log("initializing");
 const serviceAccount = JSON.parse(
     Buffer.from(process.env.SERVICE_ACCOUNT, 'base64').toString('utf8')
 );
+console.log("service account", serviceAccount);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -17,19 +19,24 @@ app.use(express.json());
 app.use(cors());
 
 const API_KEY = "test1234@K";
-
+console.log("API key");
 app.post("/sendNotification", (req, res) => {
   const authHeader = req.headers["authorization"];
-
+  console.log(authHeader);
   if (!authHeader || authHeader !== `Bearer ${API_KEY}`) {
     return res.status(401).send("Unauthorized");
   }
 
   const {token, title, body} = req.body;
+  console.log(body);
+  console.log(token);
+  console.log(title);
+
   if (!token || !title || !body) {
     return res.status(400).send("Missing token, title, or body");
   }
 
+  console.log("messaging")
   admin.messaging()
       .send({token, notification: {title, body}} )
       .then(() => res.status(200).send("Notification sent successfully"))
