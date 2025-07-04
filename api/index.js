3a8a4326e79d7dfca2cb42e89a332353e81c7a3d
ikumba01 <kumbalathara.ishara@ucy.ac.cy>
2025-07-04 10:37:31 +0300

    Added the final logs

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
 
-console.log("initializing");
+console.log("Initializing....");
+
 const serviceAccount = JSON.parse(
     Buffer.from(process.env.SERVICE_ACCOUNT, 'base64').toString('utf8')
 );
-console.log("service account", serviceAccount);
+
+console.log("Service account created...");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -19,35 +21,38 @@ app.use(express.json());
 app.use(cors());
 
 const API_KEY = "test1234@K";
-console.log("API key");
+
+console.log("API key assigned....");
 
 app.get('/', (req, res) => {
-  console.log('Received GET / request');
+  console.log('Received GET request');
   res.status(200).send('Welcome to the Push Server API.');
 });
 
 app.post("/sendNotification", (req, res) => {
   const authHeader = req.headers["authorization"];
-  console.log(authHeader);
+
+  console.log("Sent POST request");
+
   if (!authHeader || authHeader !== `Bearer ${API_KEY}`) {
     return res.status(401).send("Unauthorized");
   }
 
   const {token, title, body} = req.body;
-  console.log(body);
-  console.log(token);
-  console.log(title);
 
   if (!token || !title || !body) {
     return res.status(400).send("Missing token, title, or body");
   }
 
-  console.log("messaging")
+  console.log("Receiving the response of POST request....");
+
   admin.messaging()
       .send({token, notification: {title, body}} )
       .then(() => res.status(200).send("Notification sent successfully"))
       .catch((error) => res.status(500)
           .send(`Error sending notification: ${error}`));
 });
+
+console.log("Received the response.....");
 
 module.exports=app;
